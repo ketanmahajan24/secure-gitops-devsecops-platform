b28da2826cdce7edc0af1540f67b54e102a75504
ketanmahajan24 <ketanmahajan2003@gmail.com>
2026-01-26 01:58:49 +0530

    update terraform security_group for db server

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "master" {
 
 resource "aws_instance" "worker1" {
   ami = var.ami_id
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   subnet_id = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.k8s_worker.id]
   key_name = aws_key_pair.key.key_name
@@ -18,7 +18,7 @@ resource "aws_instance" "worker1" {
 
 resource "aws_instance" "worker2" {
   ami = var.ami_id
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   subnet_id = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.k8s_worker.id]
   key_name = aws_key_pair.key.key_name
@@ -27,7 +27,7 @@ resource "aws_instance" "worker2" {
 
 resource "aws_instance" "cicd" {
   ami = var.ami_id
-  instance_type = "t2.micro"
+  instance_type =  "t2.medium"
   subnet_id = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.cicd.id]
   key_name = aws_key_pair.key.key_name
--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -57,6 +57,13 @@ resource "aws_security_group" "cicd" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port = 3000
+    to_port   = 3000
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port = 9000
@@ -77,5 +84,30 @@ resource "aws_security_group" "cicd" {
     to_port = 0 
     protocol = "-1" 
     cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+resource "aws_security_group" "mongodb-server" {
+  name   = "mongodb-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
