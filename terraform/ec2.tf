a91b0573ffc77c6a789cd85d955a6c5a3c4578b2
ketanmahajan24 <ketanmahajan2003@gmail.com>
2026-01-27 16:15:19 +0530

    footer update

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,6 +4,13 @@ resource "aws_instance" "master" {
   subnet_id = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.k8s_master.id]
   key_name = aws_key_pair.key.key_name
+
+  
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
   tags = { Name = "k8s-master" }
 }
 
@@ -13,6 +20,13 @@ resource "aws_instance" "worker1" {
   subnet_id = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.k8s_worker.id]
   key_name = aws_key_pair.key.key_name
+
+  
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
   tags = { Name = "worker1" }
 }
 
@@ -22,7 +36,15 @@ resource "aws_instance" "worker2" {
   subnet_id = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.k8s_worker.id]
   key_name = aws_key_pair.key.key_name
+  
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
   tags = { Name = "worker2" }
+
+
 }
 
 resource "aws_instance" "cicd" {
@@ -31,5 +53,12 @@ resource "aws_instance" "cicd" {
   subnet_id = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.cicd.id]
   key_name = aws_key_pair.key.key_name
+
+  
+  root_block_device {
+    volume_size = 40
+    volume_type = "gp3"
+  }
+
   tags = { Name = "cicd-devops-node " }
 }
