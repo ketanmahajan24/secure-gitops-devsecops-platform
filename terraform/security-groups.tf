5f9cd249b0b12ad798a2af1aaf1081c95a717c82
ketanmahajan24 <ketanmahajan2003@gmail.com>
2026-02-02 22:23:03 +0530

    Add ingress for computer academy

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -7,7 +7,20 @@ resource "aws_security_group" "k8s_master" {
     protocol  = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
-  
+  ingress {
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
   ingress {
     from_port = 22
     to_port   = 22
